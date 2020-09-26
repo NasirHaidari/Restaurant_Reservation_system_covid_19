74c897632d9add2c6363b84cd9b5095062265e17
samer munawwar <samer.mnr@gmail.com>
2020-09-26 12:26:08 +0200

    added admin post request

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-
-router.get('/', index);
-
-
+router.post("/login", (req, res) => {
+    res.send({
+        status: "success",
+    });
+});
 
 module.exports = router;
