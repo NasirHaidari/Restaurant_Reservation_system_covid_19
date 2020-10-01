2190f5bf190d43e3029fe6139da27db98d8985f5
samer munawwar <samer.mnr@gmail.com>
2020-10-01 11:50:24 +0200

    get all bokkinsgs from db

--- resturant/src/components/Admin.js
+++ resturant/src/components/Admin.js
@@ -1,46 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useForm } from 'react-hook-form';
-import AdminAddRemove from './AdminAddRemove';
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import AdminAddRemove from "./AdminAddRemove";
 
 function Admin() {
     const { register, handleSubmit, errors } = useForm();
-
+    const [gotToken, setGotToken] = useState(false);
 
     const onSubmit = (data) => {
-        axios.post('http://localhost:3000/login', data)
-            .then(res => {
-                console.log(res.data)
-                localStorage.setItem('token', JSON.stringify(res.data.token.access_token));
-                console.log(localStorage.getItem('token'))
-
+        axios
+            .post("http://localhost:3000/login", data)
+            .then((res) => {
+                console.log(res.data);
+                localStorage.setItem(
+                    "token",
+                    JSON.stringify(res.data.token.access_token)
+                );
+                console.log(localStorage.getItem("token"));
+                setGotToken(true);
             })
-            .catch(err => {
-                console.log(err)
-            })
-
-    }
-
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label>Username</label>
-                <input type="text" placeholder="Username" name="username" ref={register({ required: "Username required" })} />
-                <label>Password</label>
-                <input type="password" placeholder="Password" name="password" ref={register({ required: "Password Required" })} />
-                {errors.username && <p>{errors.username.message}</p>}
-                {errors.password && <p>{errors.password.message}</p>}
+            {gotToken ? (
+                <AdminAddRemove />
+            ) : (
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label>Username</label>
+                    <input
+                        type='text'
+                        placeholder='Username'
+                        name='username'
+                        ref={register({ required: "Username required" })}
+                    />
+                    <label>Password</label>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        ref={register({ required: "Password Required" })}
+                    />
+                    {errors.username && <p>{errors.username.message}</p>}
+                    {errors.password && <p>{errors.password.message}</p>}
 
-                <input type="submit" />
-
-            </form>
-            <AdminAddRemove />
-
+                    <input type='submit' />
+                </form>
+            )}
         </>
-    )
+    );
 }
 
-export default Admin
+export default Admin;
--- resturant/src/components/AdminAddRemove.js
+++ resturant/src/components/AdminAddRemove.js
@@ -1,39 +1,102 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function AdminAddRemove() {
-    const { key, setKey } = useState('');
+    const [key, setKey] = useState("");
+    const [bookings, setBookings] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
     const navigate = useNavigate();
-    const handleClick = () => navigate('/search');
+    const handleClick = () => navigate("/search");
 
+    useEffect(() => {
+        const token = JSON.parse(localStorage.getItem("token"));
+        const config = {
+            headers: { Authorization: `Bearer ${token}` },
+        };
+        console.log(token);
 
+        axios.get("http://localhost:3000/admin", config).then((res) => {
+            console.log(res.data);
+            setBookings(res.data.data);
+        });
+    }, []);
 
     const removeHandle = (e) => {
         e.preventDefault();
-        axios.delete('http://localhost:3000/admin/', key)
-
-    }
+        axios.delete("http://localhost:3000/admin/", key);
+    };
 
     const editHandle = () => {
-        console.log('editing')
-    }
+        console.log("editing");
+    };
 
+    const handleSearch = (name) => {
+        const NewData = bookings.filter((booking) =>
+            booking.name.includes(name)
+        );
+
+        setBookings(NewData);
+    };
 
     return (
-        <div>
-            <button onCkick={editHandle}>Edit</button>
+        <div className='container'>
+            <button onClick={editHandle}>Edit</button>
             <form onSubmit={removeHandle}>
                 <label>booking number for remove a booking</label>
-                <input type="text" onChange={e => setKey(e.target.value)}
-                    placeholder="input your bookings number here and click the remove button" />
+                <input
+                    type='text'
+                    onChange={(e) => setKey(e.target.value)}
+                    placeholder='input your bookings number here and click the remove button'
+                />
             </form>
             <button onClick={handleClick}>make a reservation</button>
-
-
-
+            {bookings ? (
+                <div>
+                    <div class='input-group mb-3'>
+                        <input
+                            onChange={(e) => {
+                                setSearchInput(e.target.value);
+                                handleSearch(e.target.value);
+                            }}
+                            type='text'
+                            class='form-control'
+                            placeholder="Recipient's username"
+                            aria-label="Recipient's username"
+                            aria-describedby='button-addon2'
+                            value={searchInput}
+                        />
+                        <div class='input-group-append'>
+                            <button
+                                class='btn btn-outline-success'
+                                type='button'
+                                id='button-addon2'
+                            >
+                                Button
+                            </button>
+                        </div>
+                    </div>
+                    <ul className='list-group'>
+                        {bookings.map((booking) => {
+                            return (
+                                <li className='list-group-item list-group-item-secondary d-flex justify-content-between mb-1'>
+                                    {booking.name}
+                                    <div>
+                                        <button className='btn btn-info'>
+                                            Edit
+                                        </button>
+                                        <button className='btn btn-danger ml-1'>
+                                            Remove
+                                        </button>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            ) : (
+                ""
+            )}
         </div>
-    )
+    );
 }
