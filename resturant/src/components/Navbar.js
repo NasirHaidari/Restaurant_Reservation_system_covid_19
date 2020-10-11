e918f557533f7fbcbed8f7a0c3d30eee141289c7
cod01 <nasir.haidari@hotmail.com>
2020-10-11 18:29:37 +0200

    added time function and fetched reservetion for today

--- resturant/src/components/Navbar.js
+++ resturant/src/components/Navbar.js
@@ -2,42 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <>
-            <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
-                <Link className='navbar-brand' to='/'>
-                    Navbar
-                </Link>
-                <button
-                    className='navbar-toggler'
-                    type='button'
-                    data-toggle='collapse'
-                    data-target='#navbarNavAltMarkup'
-                    aria-controls='navbarNavAltMarkup'
-                    aria-expanded='false'
-                    aria-label='Toggle navigation'
-                >
-                    <span className='navbar-toggler-icon'></span>
-                </button>
-                <div
-                    className='collapse navbar-collapse'
-                    id='navbarNavAltMarkup'
-                >
-                    <div className='navbar-nav'>
-                        <Link to='/' className='nav-link active'>
-                            Home <span className='sr-only'>(current)</span>
-                        </Link>
-                        <Link to='/search' className='nav-link'>
-                            booking
-                        </Link>
-                        <Link to='/login' className='nav-link'>
-                            Login
-                        </Link>
-                    </div>
-                </div>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link className="navbar-brand" to="/">
+          SKY
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link to="/" className="nav-link active">
+              Home <span className="sr-only">(current)</span>
+            </Link>
+            <Link to="/search" className="nav-link">
+              booking
+            </Link>
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
